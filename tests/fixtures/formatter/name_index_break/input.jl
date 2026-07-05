98be310622c496c230a79d64d2a24ced44f93e14
Johan Larsson <johan@jolars.co>
2026-07-05 16:45:05 +0900

    test(formatter): gate name-rooted chain break
    11 cases: short dotted chain, 92/93 fence pair, wide-first with two
    riding tails, small-first/wide-second tradeoff, dotted root, hugged
    innermost with riding tail, both links overflowing, single wide index
    regression, pre-broken input reflowing flat.

---
+++ tests/fixtures/formatter/name_index_break/input.jl
@@ -0,0 +1,13 @@
+smalldot = config.table[k][j]
+fits_at_ninety_two = registry[alpha_selector_component, beta_selector_component][the_keysxx]
+breaks_at_ninety_three = registry[alpha_selector_component, beta_selector_component][key_xyz]
+chain = lookup_table[first_rather_long_index_expression, second_long_index_expression][key_one][key_two]
+small_first = lookup_table[key][some_extremely_long_key_expression, another_quite_long_key_expression]
+dotted = config.lookup_table[some_extremely_long_key_expression, another_long_key_expression][ky]
+hugged = handlers[event_key_selector, [alpha_handler_entry, beta_handler_entry, gamma_handler_entry]][idx]
+both = wide_lookup_table[first_quite_long_index_expression, second_quite_long_index_expression][third_extremely_long_selector_expression_one, fourth_extremely_long_selector_expression_two]
+single = coefficient_lookup[some_extremely_long_key_expression, another_quite_long_key_exprs]
+reflowed = registry[
+    alpha_component,
+    beta_component,
+][key]
