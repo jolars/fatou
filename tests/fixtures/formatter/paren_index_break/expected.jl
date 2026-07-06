3f494228ba24a782c82168e8a078ed2d7af5af2f
Johan Larsson <johan@jolars.co>
2026-07-06 09:10:58 +0900

    test(formatter): gate paren index subject break

---
+++ tests/fixtures/formatter/paren_index_break/expected.jl
@@ -0,0 +1,31 @@
+small = (a + b)[k]
+fence92 = (f(alpha_selector_component, beta_selector_component, gamma_comp))[the_keys_heree]
+fence93 = (
+    f(alpha_selector_component, beta_selector_component, gamma_comp)
+)[the_keys_hereee]
+call_subject = (
+    f(some_long_argument_name, another_long_argument_here, third_argument_xx)
+)[some_index_key_here]
+binary_subject = (
+    a_long_value + another_long_value + a_third_operand_value_here_yes_indeed
+)[first_index][second_key]
+deep_inner = (
+    f(
+        some_really_long_argument_name_one,
+        some_really_long_argument_name_two,
+        some_really_long_argument_name_three,
+    )
+)[some_index_key_here]
+nested_paren = (
+    (f(some_long_argument_name, another_long_argument_here, third_argument_xxyz))
+)[some_index_key_here]
+wide_index = (
+    a_value + b_value
+)[some_extremely_long_index_expression_one, another_extremely_long_index_expr_two]
+both_overflow = (
+    a_long_value + another_long_value
+)[
+    first_extremely_long_index_expression_component_one,
+    second_extremely_long_index_expression_component_two,
+]
+reflowed = (f(alpha_component, beta_component))[key]
