c22834ea213c2c81afcdec2a3643e33ff3ebdfc0
Johan Larsson <johan@jolars.co>
2026-08-25 14:39:39 +0200

    test(formatter): gate left division

---
+++ crates/fatou-formatter/tests/fixtures/formatter/left_division/expected.jl
@@ -0,0 +1,10 @@
+x = A \ b
+y = A \ b
+solution = lower_triangular_factorization \
+    first_right_hand_side \
+    second_right_hand_side \
+    third_right_hand_side
+mixed_result = first_long_matrix_operand *
+    second_long_matrix_operand \
+    third_long_matrix_operand /
+    fourth_long_matrix_operand
